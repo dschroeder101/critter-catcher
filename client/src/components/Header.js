85fc09605296b4f19ebe5c8d630ec5524aa6dbf0
Daniel Schroeder <dschroeder101@gmail.com>
2020-06-26 14:47:41 -0700

    Updated title and info strings

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
         return (
             <div>
                 <header className="App-header">
-                    <h1>Welcome to critter-catcher!</h1>
+                    <h1>Welcome to current-critters!</h1>
                     <h4>Select a hemisphere below to see which critters are currently available for you to catch in Animal Crossing: New Horizons</h4>
                 </header>
             </div>
